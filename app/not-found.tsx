6254ef3c621531c5d45d67d228aeb4bb477219ee
Hugo Loiola <hugoloiola.1@gmail.com>
2024-09-13 00:31:05 -0300

    mudanças

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Erro 404</h1>
+      <h3>Página não encontrada!</h3>
+      <img src="" />
+    </div>
+  );
+};
+
+export default NotFound;
