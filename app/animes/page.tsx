4e9ea74d87c7fa9b3a311ac4a5918a5dd66f0af9
Hugo Loiola <hugoloiola.1@gmail.com>
2024-09-16 19:30:42 -0300

    TS chatao

--- app/animes/page.tsx
+++ app/animes/page.tsx
@@ -5,7 +5,17 @@ export default async function Page() {
   const response = await fetch("https://api.jikan.moe/v4/seasons/now");
   const data = await response.json();
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   return <ul>
-	{data.data.map((item) => <li key={item.mal_id}><h1 className="text-center text-white"> {item.title}</h1></li>)}
+	{/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+	{data.data.map((item: {
+	  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	  mal_id: React.Key | null | undefined; // @ts-expect-error
+	  title: string | number | bigint | boolean | React.ReactElement<never, string | React.JSXElementConstructor<never>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<React.AwaitedReactNode> | null | undefined;
+	}) => {
+	  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	  // @ts-expect-error
+	  return <li key={item.mal_id}><h1 className="text-center text-white"> {item.title}</h1></li>;
+	})}
   </ul>;
 }
