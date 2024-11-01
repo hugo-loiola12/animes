30961454b87cc72a6130853319baef98a8a6faeb
Hugo Loiola <hugoloiola.1@gmail.com>
2024-10-31 23:40:15 -0300

    Ajustes Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,84 +6,88 @@ const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-      <nav className="bg-white border-gray-200 dark:bg-gray-900 font-titulo">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <Link href="/" aria-label="AnimesHugo Home" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img
-                src="https://flowbite.com/docs/images/logo.svg"
-                className="h-8"
-                alt="Flowbite Logo"
-            />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+    <nav className="bg-white border-gray-200 dark:bg-gray-900 font-titulo">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link
+          href="/"
+          aria-label="AnimesHugo Home"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="h-8"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             AnimesHugo
           </span>
-          </Link>
+        </Link>
 
-          {/* Menu Toggle Button */}
-          <button
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="navbar-default"
-              aria-expanded={isMenuOpen ? "true" : "false"}
-              aria-label="Open main menu"
+        {/* Botão do menu de hambúrguer */}
+        <button
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded={isMenuOpen ? "true" : "false"}
+          aria-label="Open main menu"
+        >
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
           >
-            <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
-            >
-              <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
 
-          {/* Navigation Menu */}
-          <div
-              className={`${
-                  isMenuOpen ? "block opacity-100 translate-y-0" : "hidden opacity-0 -translate-y-4"
-              } transition-all duration-300 ease-in-out md:block w-full md:w-auto`}
-              id="navbar-default"
-          >
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
-                <Link
-                    href="/"
-                    aria-label="Home"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                    aria-current="page"
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                    href="/animes"
-                    aria-label="Animes"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Animes
-                </Link>
-              </li>
-              <li>
-                <Link
-                    href="/mangas"
-                    aria-label="Mangas"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Mangas
-                </Link>
-              </li>
-            </ul>
-          </div>
+        {/* Menu de navegação */}
+        <div
+          className={`${
+            isMenuOpen ? "block" : "hidden"
+          } w-full md:block md:w-auto`}
+          id="navbar-default"
+        >
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <Link
+                href="/"
+                aria-label="Home"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                aria-current="page"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/animes"
+                aria-label="Animes"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Animes
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/mangas"
+                aria-label="Mangas"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Mangas
+              </Link>
+            </li>
+          </ul>
         </div>
-      </nav>
+      </div>
+    </nav>
   );
 };
 
--- app/mangas/page.tsx
+++ app/mangas/page.tsx
@@ -2,35 +2,35 @@ import React from "react";
 import Link from "next/link";
 import Card from "@/app/components/Card";
 
-export default async function index(){
+export default async function index() {
   // Buscando dados da API Jikan
   const response = await fetch("https://api.jikan.moe/v4/top/manga");
   const data = await response.json();
 
   return (
-      <div className="flex flex-wrap justify-evenly gap-4 py-8">
-        {data.data.map(
-            (item: {
-              year: never;
-              score: never;
-              mal_id: React.Key | null | undefined;
-              title: never;
-              synopsis: never;
-              images: { jpg: { large_image_url: never } };
-            }) => (
-                <Link href={`/mangas/${item.mal_id}`} key={item.mal_id}>
-                  {/* Garantir que o conteúdo dentro de Link seja apenas um elemento */}
-                  <div>
-                    <Card
-                        titulo={item.title}
-                        imagem={item.images.jpg.large_image_url}
-                        data={item.year}
-                        nota={item.score}
-                    />
-                  </div>
-                </Link>
-            )
-        )}
-      </div>
+    <div className="flex flex-wrap justify-evenly gap-4 py-8">
+      {data.data.map(
+        (item: {
+          year: never;
+          score: never;
+          mal_id: React.Key | null | undefined;
+          title: never;
+          synopsis: never;
+          images: { jpg: { large_image_url: never } };
+        }) => (
+          <Link href={`/mangas/${item.mal_id}`} key={item.mal_id}>
+            {/* Garantir que o conteúdo dentro de Link seja apenas um elemento */}
+            <div>
+              <Card
+                titulo={item.title}
+                imagem={item.images.jpg.large_image_url}
+                data={item.year}
+                nota={item.score}
+              />
+            </div>
+          </Link>
+        )
+      )}
+    </div>
   );
-};
+}
